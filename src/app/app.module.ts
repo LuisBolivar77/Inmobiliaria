67b74567d2ec771cd8cc7d905cb6b259ecbf39b8
CarlosM4rtinez <inf.carlosmartinez@gmail.com>
2018-09-13 20:17:38 -0500

    Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { LoginComponent } from './Paginas/login/login.component';
+import { IndexComponent } from './Paginas/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent
+    LogInComponent,
+    LoginComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +24,17 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    RouterModule.forRoot([
+      {
+        path: '', 
+        component: IndexComponent
+      },
+      {
+        path: 'login', // http://localhost:4200/login
+        component: LoginComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
