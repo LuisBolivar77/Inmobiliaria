5ed04268f02b0959fcd97970d411336276b88db9
LauraPosada <posadalaura998@gmail.com>
2018-09-29 19:20:42 -0500

    cambio en app.js de contraseña de bd

--- src/app/Paginas/seguro/gestionar-personas/gestionar-personas.component.spec.ts
+++ src/app/Paginas/seguro/gestionar-personas/gestionar-personas.component.spec.ts
@@ -14,9 +14,22 @@ import {
 import { not } from '@angular/compiler/src/output/output_ast';
 import { HttpClientModule } from '@angular/common/http';
 import { GestionarPersonasComponent } from './gestionar-personas.component';
+import { NgForm, FormsModule } from '@angular/forms';
 
 fdescribe('GestionarPersonasComponent', () => {
-  // Rol que tendra la persona
+  
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers:    [ GenericoService ],
+      imports: [ HttpClientModule, FormsModule ],
+      declarations:[GestionarPersonasComponent]
+    });
+
+  });
+
+  it('crear una persona', () => {
+    // Rol que tendra la persona
   // tslint:disable-next-line:prefer-const
   let rol = new Rol();
   // la informacion de la persona asociada al usuario
@@ -24,34 +37,20 @@ fdescribe('GestionarPersonasComponent', () => {
   let persona = new Persona();
   // usuario que se registra con la persona
   // tslint:disable-next-line:prefer-const
-  let usuario = new Usuario();
-
-  
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers:    [ GenericoService ],
-      imports: [ HttpClientModule ],
-      declarations:[GestionarPersonasComponent]
-    });
-
-  });
-
- 
+  let usuario = new Usuario();  
+    console.log("POR AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII itititititititiit");
     
-
-
-  it('crear una persona', () => {
-
     rol.id = 1;
     rol.nombre = 'Administrador';
     rol.descripcion = 'administrador';
 
     // datos de la persona
+    persona.id=7;
     persona.cedula = '123456789';
     persona.nombre = 'gaga';
     persona.apellido = 'posada';
-    persona.fecha_nacimiento = '1997-10-10';
+      let fecha = new Date('1997-03-03');
+    persona.fecha_nacimiento = fecha;
     persona.telefono = '3134566545';
     persona.direccion = 'calle 45';
     persona.rol = rol;
@@ -60,19 +59,21 @@ fdescribe('GestionarPersonasComponent', () => {
     usuario.username = 'laura';
     usuario.password = '123';
 
-        // Usamos TestBed para poder usar el servicio http
+    console.log(persona + " HOLAAAAAAAAAAAAAAAAAA POR AQUIIIIIIIII");
 
+     // Usamos TestBed para poder usar el servicio http
     const servicio: GenericoService = TestBed.get(GenericoService);
-
+      
     // Perform a request and make sure we get the response we expect
-    servicio.registrar("personas",{usuario}).subscribe(rta => {
+    servicio.registrar("personas",{"cedula":persona}).subscribe(rta => {
       console.log("ENTROOOOOOOOOOOOOOOOOOOOOOOOOO");
     //console.log(usuario);
-     // expect(rta.data.length).toEqual(1);
-     servicio.buscar("personas", {"id":persona.cedula}).subscribe(rtap => {
+    console.log(rta);
+    //  expect(rta.data.length).toEqual(1);
+    // servicio.buscar("personas", {"id":persona.cedula}).subscribe(rtap => {
       // Validamos si la respuesta si concuerda con la esparada      
-      expect(rtap.cedula).toEqual('123456789');
-    });
+     // expect(rtap.cedula).toEqual('123456789');
+   // });
 
     });
   });
