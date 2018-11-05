4696b2d96910da9b1a761f48749a20bc22bd410b
Daniela5579 <daniela.rojas.5579@eam.edu.co>
2018-11-05 11:17:08 -0500

    first commit

--- src/app/Paginas/seguro/asignar-ventas-contratos/asignar-ventas-contratos.component.spec.ts
+++ src/app/Paginas/seguro/asignar-ventas-contratos/asignar-ventas-contratos.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AsignarVentasContratosComponent } from './asignar-ventas-contratos.component';
+import { GenericoService } from '../../../Servicios/genericoServ.service';
+import { HttpClientModule } from '../../../../../node_modules/@angular/common/http';
+import { FormsModule } from '../../../../../node_modules/@angular/forms';
+import { RouterTestingModule } from '../../../../../node_modules/@angular/router/testing';
+import { Contrato } from '../../../Modelo/Contrato';
+import { Empleado } from '../../../Modelo/Empleado';
+import { Persona } from '../../../Modelo/Persona';
+import { ReservarVisita } from '../../../Modelo/ReservarVisita';
 
 describe('AsignarVentasContratosComponent', () => {
   let component: AsignarVentasContratosComponent;
@@ -8,9 +16,13 @@ describe('AsignarVentasContratosComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AsignarVentasContratosComponent ]
-    })
-    .compileComponents();
+      // el servicio a usar
+      providers: [GenericoService],
+      // Importamos el http para poder consumir los servicios
+      imports: [HttpClientModule, FormsModule, RouterTestingModule],
+      // Se declara el componente, para poder ver el reporte en el coverage
+      declarations: [AsignarVentasContratosComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +31,15 @@ describe('AsignarVentasContratosComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('buscar contrato', () => {
+
+    console.log("ENTRO BUSCAR CONTRATO");   
+    
+    let contrato: Contrato = new Contrato();
+    contrato.id =2;
+    component.contrato.id = 2;
+    
+    const buscar = component.buscarContrato();
+    expect(buscar).toBeTruthy;
   });
 });
--- src/app/Paginas/seguro/gestion-ventas-arriendos/gestion-ventas-arriendos.component.spec.ts
+++ src/app/Paginas/seguro/gestion-ventas-arriendos/gestion-ventas-arriendos.component.spec.ts
@@ -1,16 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GestionVentasArriendosComponent } from './gestion-ventas-arriendos.component';
+import { GenericoService } from '../../../Servicios/genericoServ.service';
+import { HttpClientModule } from '../../../../../node_modules/@angular/common/http';
+import { FormsModule } from '../../../../../node_modules/@angular/forms';
+import { RouterTestingModule } from '../../../../../node_modules/@angular/router/testing';
+import { Contrato } from '../../../Modelo/Contrato';
+import { Persona } from '../../../Modelo/Persona';
+import { ReservarVisita } from '../../../Modelo/ReservarVisita';
+import { Usuario } from '../../../Modelo/Usuario';
 
-describe('GestionVentasArriendosComponent', () => {
+fdescribe('GestionVentasArriendosComponent', () => {
   let component: GestionVentasArriendosComponent;
   let fixture: ComponentFixture<GestionVentasArriendosComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GestionVentasArriendosComponent ]
-    })
-    .compileComponents();
+      // el servicio a usar
+      providers: [GenericoService],
+      // Importamos el http para poder consumir los servicios
+      imports: [HttpClientModule, FormsModule, RouterTestingModule],
+      // Se declara el componente, para poder ver el reporte en el coverage
+      declarations: [GestionVentasArriendosComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +31,31 @@ describe('GestionVentasArriendosComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('editar contrato', () => {
+
+    console.log("ENTRO editar CONTRATO gestion");
+    
+
+    let persona= new Persona();
+    persona.id = 3;
+    
+    let empleado = new Usuario();
+
+    let cliente = new Usuario();
+    cliente.persona = 
+
+    let visita = new ReservarVisita();
+
+    component.usuarioSesion= component.usuarioSesion;
+
+    component.contrato.id =2;
+    component.contrato.descripcion='cualquier cosa';
+    component.contrato.visita = visita;
+
+    
+    const editar = component.editar(null);
+    expect(editar).toBeTruthy;
   });
+
+  
 });
